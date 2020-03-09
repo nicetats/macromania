36a2ffef3a75bfc82a32284f83589336f5a98af7
nicetats <61529845+nicetats@users.noreply.github.com>
2020-03-09 09:46:49 -0400

    initial commit of draft of final project

---
+++ Storyline/Macro Mania - Storyline output/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68dsGDGep3K":
+        Script1();
+        break;
+      case "6TiHxhsjLgO":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
+}
+
